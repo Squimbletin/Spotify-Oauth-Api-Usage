87e0ea447d278044f16f4a53f3dd1646734b5a6f
Squimbletin <144500641+Squimbletin@users.noreply.github.com>
2024-06-06 12:44:08 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'flask',
+    'spotipy',
+    'html5lib',
+    'requests',
+    'requests_html',
+    'beautifulsoup4',
+    'youtube_dl',
+    'pathlib',
+    'pandas'
+]
+
+setup(
+    name='SpotifyToYoutubeMP3',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires
+)
